c7b62efebcec0b538e9c5efbdf1d4b8160c2ca22
100103067 <100103067@lubbockisd.net>
2019-10-14 15:54:22 -0500

    2nd commit of 10/14

--- week of 10/14.py
+++ week of 10/14.py
@@ -19,7 +19,14 @@ def archimedes(numSides):
 
 print(archimedes(4))
 print(archimedes(8))
-print(archimedes(16))
+print(archimedes(360000000))
 
 for sides in range(8, 100, 8):
     print(sides, archimedes(sides))
+
+# See the loop above. In addition to the value of pi, print the difference
+# between the values calculated by the archimedes function and by math.pi.
+# How many sides does it take to make the two close?
+# It takes 40, 400, 40 to make the Archimedes close.
+
+print(math.pi)
