1c7df355100822e818d65b6a423a550cdc6e18e0
100103067 <100103067@lubbockisd.net>
2019-10-14 15:35:22 -0500

    first commit of 10/14

---
+++ week of 10/14.py
@@ -0,0 +1,25 @@
+# We are going to look at approximations of Pi
+# as well as looking at the math Module
+
+print(22/7)
+print(355/113)
+
+import math
+
+print(9801 / (2206 * math.sqrt(2)))
+
+def archimedes(numSides):
+    innerAngleB = 360.0 / numSides
+    halfAngleA = innerAngleB / 2
+    oneHalfSides = math.sin(math.radians(halfAngleA))
+    sides = oneHalfSides / 2
+    polygonCircumference = numSides * sides
+    pi = polygonCircumference / 2
+    return pi
+
+print(archimedes(4))
+print(archimedes(8))
+print(archimedes(16))
+
+for sides in range(8, 100, 8):
+    print(sides, archimedes(sides))
