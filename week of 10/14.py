4fdf0304f390b5dc7b8ef27d2af9c8c7d3486b39
100103067 <100103067@lubbockisd.net>
2019-10-22 15:38:54 -0500

    added montePi function

--- week of 10/14.py
+++ week of 10/14.py
@@ -219,6 +219,51 @@ def montePi(numDarts):
     pi = inCircle / numDarts * 4
     return pi
 
+print(montePi(1000))
 
+
+import turtle
+
+def showMontePi(numDarts):
+    scn = turtle.Screen()
+    t = turtle.Turtle()
+
+    scn.setworldcoordinates(-2, -2, 2, 2)
+
+    t.penup()
+    t.goto(-1, 0)
+    t.pendown()
+    t.goto(1, 0)
+
+    t.penup()
+    t.goto(0, 1)
+    t.pendown()
+    t.goto(0, -1)
+
+
+    inCircle = 0
+    t.penup()
+
+    for i in range(numDarts):
+        x = random.random()
+        y = random.random()
+
+        distance = math.sqrt(x**2 + y**2)
+        t.goto(x, y)
+
+
+        if distance <= 1:
+            inCircle = inCircle + 1
+            t.color("pink")
+        else:
+            t.color("light blue")
+
+        t.dot()
+
+    pi = inCircle / numDarts * 4
+    scn.exitonclick()
+    return pi
+
+print()
 
 
